5acc9ffd41d67467c9adf2acbfbf0604af9835e6
Adnan Sakr <adnansakr593-beep>
2025-12-28 14:24:29 +0200

    ..

--- lib/widgets/noinfo.dart
+++ lib/widgets/noinfo.dart
@@ -7,19 +7,20 @@ class Noinfo extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
-        padding: EdgeInsets.only(left: 50),
+        padding: EdgeInsets.only(left: 35),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text
             (
               'There is no weather 😶‍🌫️ ', 
-              style: TextStyle(fontSize: 30),
+              style: TextStyle(fontSize: 28),
             ),
             Text
             (
               "Start Searching Now 🔍 ", 
-              style: TextStyle(fontSize: 30),
+              style: TextStyle(fontSize: 28),
             ),
           ],
         ),
